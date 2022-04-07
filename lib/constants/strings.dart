88ebccff53198ca25d5c5ec50ed05e4fcd217eee
Akash Mondal <a98mondal@gmail.com>
2022-04-08 01:46:08 +0530

    Configure html as default flutter web renderer
    * Fixes image render error due to Cross-Origin Resource Sharing (CORS)
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,3 +1,3 @@
-const flaavnApiBase = 'http://localhost:3000/api';
+const flaavnApiBase = 'https://flaavn.vercel.app/api';
 const apiUserAgent =
     'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0';
--- lib/widgets/image_display.dart
+++ lib/widgets/image_display.dart
@@ -1,14 +1,10 @@
-import 'package:flaavn/constants/strings.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ImageDisplay extends StatelessWidget {
   final String url;
   final BorderRadius? borderRadius;
 
-  const ImageDisplay(String url, {Key? key, this.borderRadius})
-      : url = kIsWeb ? flaavnApiBase + '/proxy?url=' + url : url,
-        super(key: key);
+  const ImageDisplay(this.url, {Key? key, this.borderRadius}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
